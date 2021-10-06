a33445b9ed3f1c2cbb1902b929f3935f25a41a1c
Dastdjc <90397187+Dastdjc@users.noreply.github.com>
2021-10-06 13:01:58 +0200

    2.El sistema Physics

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rb;
+    
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Launch(Vector2 direction, float force)
     {
-        
+        rb.AddForce(direction * force);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //we also add a debug log to know what the projectile touch
+        Debug.Log("Projectile Collision with " + other.gameObject);
+        Destroy(gameObject);
     }
 }
